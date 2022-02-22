84011c6be312b92a1510deb5cf9233aab1b501d6
vadsolid <maguscode@protonmail.com>
2022-02-21 22:03:12 -0500

    init: resolve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+   let result = []
+   if (matrix) {matrix.map((arr, i ) => {
+       return (i%2) ? result = result.concat(arr.reverse()) : result = result.concat(arr);
+    })}
+   return result
 }
